a480e2f92fe1a96415f77a4f3bd70127f378a612
David Larson <sage7one@gmail.com>
2026-06-06 09:21:26 -0500

    feat: baked-in fallback snapshot (2026-06-06)

---
+++ snapshot.js
@@ -0,0 +1,80 @@
+export const BAKED_SNAPSHOT = {
+  "asOf": "2026-06-06",
+  "hl": {
+    "mark": 164.82,
+    "oracle": 164.89,
+    "prevDay": 162.21,
+    "funding": -2.305e-7
+  },
+  "curve": [
+    {
+      "capT": 1,
+      "pAbove": 99.05000000000001
+    },
+    {
+      "capT": 1.2,
+      "pAbove": 97.45
+    },
+    {
+      "capT": 1.4,
+      "pAbove": 96.3
+    },
+    {
+      "capT": 1.6,
+      "pAbove": 90.4
+    },
+    {
+      "capT": 1.8,
+      "pAbove": 76.5
+    },
+    {
+      "capT": 2,
+      "pAbove": 63.5
+    },
+    {
+      "capT": 2.2,
+      "pAbove": 44.5
+    },
+    {
+      "capT": 2.4,
+      "pAbove": 30.5
+    },
+    {
+      "capT": 2.6,
+      "pAbove": 15.5
+    },
+    {
+      "capT": 2.8,
+      "pAbove": 8.5
+    },
+    {
+      "capT": 3,
+      "pAbove": 5.5
+    },
+    {
+      "capT": 3.2,
+      "pAbove": 3.25
+    },
+    {
+      "capT": 3.4,
+      "pAbove": 2.15
+    },
+    {
+      "capT": 3.6,
+      "pAbove": 1.7999999999999998
+    },
+    {
+      "capT": 3.8,
+      "pAbove": 1.7500000000000002
+    },
+    {
+      "capT": 4,
+      "pAbove": 1
+    }
+  ],
+  "vol": {
+    "hourlySigma": 0.012254045325531567,
+    "sessionSigma": 0.03124180811766795,
+    "sliderVal": 6
+  }
+};
